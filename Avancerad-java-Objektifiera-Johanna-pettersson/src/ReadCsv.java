2598a329ec0b8a70a817d1cbaf93f46548555e05
poooootiful <johanna_pettersson@live.com>
2023-12-01 14:22:59 +0100

    nice

--- Avancerad-java-Objektifiera-Johanna-pettersson/src/ReadCsv.java
+++ Avancerad-java-Objektifiera-Johanna-pettersson/src/ReadCsv.java
@@ -1,11 +1,47 @@
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ReadCsv extends Gui{
-    JFileChooser f = new JFileChooser();
+
     JTable table = new JTable();
 
-    ReadCsv () {
-        System.out.println("Csv");
+    int Colums =0;
+    int Rows =0;
 
+    Scanner Read;
+
+    ReadCsv () {
+
+        System.out.println("Csv");
+        JFileChooser fileChooser = new JFileChooser("src");
+        int option = fileChooser.showOpenDialog(null);
+
+        switch (option) {
+            case JFileChooser.APPROVE_OPTION -> System.out.println("Open");
+            case JFileChooser.CANCEL_OPTION -> System.out.println("Cancel");
+            case JFileChooser.ERROR_OPTION -> System.out.println("Error");
+        }
+
+        try {
+            String path = fileChooser.getSelectedFile().toString();
+            String filetype = path.split(",");
+            File file = fileChooser.getSelectedFile();
+            String fileDec = fileChooser.getTypeDescription(file);
+            Read = new Scanner(file);
+            System.out.println(path);
+            System.out.println(filetype);
+            System.out.println(fileDec);
+        }catch (Exception e) {
+            System.out.println("No file picked"+" "+e);
+        }
+
+        if (Read.hasNextLine()) {
+            Colums  = Read.nextLine().split(",").length;
+        }
+
+        Read.close();
+        System.out.println(Colums);
     }
 }
