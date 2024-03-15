763a1fb7c87d5024da185e84e55d7f033183ffe4
poooootiful <johanna_pettersson@live.com>
2024-03-15 14:38:42 +0100

    help

--- Avancerad-java-Objektifiera-Johanna-pettersson/src/ReadCsv.java
+++ Avancerad-java-Objektifiera-Johanna-pettersson/src/ReadCsv.java
@@ -2,16 +2,29 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class ReadCsv extends Gui {
 
-    JFrame frame = new JFrame("csv");
+    JFrame frame;
     JTable table;
+    String [][] data = {};
+
 
     ReadCsv () throws IOException {
 
         System.out.println("Csv");
-        /*JFileChooser fileChooser = new JFileChooser("src");
+        JFileChooser fileChooser = new JFileChooser("src");
         fileChooser.addChoosableFileFilter(new FileNameExtensionFilter ("csv file (.csv)","csv"));
         int option = fileChooser.showOpenDialog(null);
 
@@ -19,25 +32,35 @@ public class ReadCsv extends Gui {
             case JFileChooser.APPROVE_OPTION -> System.out.println("Open");
             case JFileChooser.CANCEL_OPTION -> System.out.println("Cancel");
             case JFileChooser.ERROR_OPTION -> System.out.println("Error");
-        }*/
+        }
 
-        String file = "src\\Sample.csv";
-        BufferedReader reader= null;
-        String line = "";
-
+        String file = fileChooser.getSelectedFile().toString();
+        BufferedReader reader = null;
+        String line =  "";
+        String [] row;
+        
         try {
             reader = new BufferedReader(new FileReader(file));
-
-            while ((line = reader.readLine())!= null) {
-
-                String [] row = line.split(",");
-
+            
+            while ((line=reader.readLine())!=null) {
+                row = line.split(",");
                 for (String index : row) {
                     System.out.printf("%-10s", index);
                 }
-
                 System.out.println();
             }
+
+            String [] columnNames = {"OrderDate", "Region", "Rep1", "Rep2", "Item", "Units", "UnitCost", "Total"};
+
+            frame = new JFrame("Csv");
+
+            table = new JTable(data,columnNames);
+            table.setBounds(30,40,200,300);
+
+            JScrollPane sp = new JScrollPane();
+            frame.add(sp);
+            frame.setSize(500,200);
+            frame.setVisible(true);
 
         }catch (Exception e) {
             e.printStackTrace();
--- Avancerad-java-Objektifiera-Johanna-pettersson/src/ReadJson.java
+++ Avancerad-java-Objektifiera-Johanna-pettersson/src/ReadJson.java
@@ -2,6 +2,9 @@ public class ReadJson extends Gui{
 
     ReadJson () {
         System.out.println("Json");
+
+
+
     }
 
 }
